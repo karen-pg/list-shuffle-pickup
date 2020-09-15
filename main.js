4c24c5dc3cb416855e00b81197be192827d5cb12
karen-pg <flower.pg.0713@gmail.com>
2020-09-15 14:13:20 +0900

    グループ分け機能を追加、レスポンシブ対応

--- main.js
+++ main.js
@@ -6,18 +6,11 @@ const shuffleWrapper = document.querySelector("#shuffle-wrapper");
 // ピックアップ数確認
 const pickup = document.querySelector("#pickup");
 function sortingCheckbox(eitherArray, pickNum) {
-  if (
-    pickNum >= eitherArray.length
-      ? alert("リストの数より小さい数字を指定してください。")
-      : shuffle(eitherArray, pickup.textContent, pickNum)
-  );
+  pickNum >= eitherArray.length
+    ? alert("リストの数より小さい数字を指定してください。")
+    : shuffle(eitherArray, pickup.textContent, pickNum);
 }
 
-// 乱数
-function randomNum(array) {
-  const r = Math.floor(Math.random() * array.length);
-  return r;
-}
 //シャッフル
 function shuffle(array, src, pickNum) {
   const randomArray = [...array];
@@ -27,16 +20,19 @@ function shuffle(array, src, pickNum) {
   }
   switch (src) {
     case "シャッフル":
-      if (
-        array === lists
-          ? listInsert(randomArray, shuffleWrapper, "sh")
-          : listInsert(randomArray, pickupWrapper, "pk")
-      );
+      array === lists
+        ? listInsert(randomArray, shuffleWrapper, "sh")
+        : listInsert(randomArray, pickupWrapper, "pk");
       break;
     case "ピックアップ":
       listPickup(randomArray, pickNum);
       break;
   }
+}
+// 乱数
+function randomNum(array) {
+  const r = Math.floor(Math.random() * array.length);
+  return r;
 }
 
 // ピックアップ発火準備
@@ -66,13 +62,37 @@ const elementPrepare = (list) => {
 // ピックアップされたリストを生成
 const sum = document.querySelector("#sum");
 function listInsert(picklist, wrapper, cls = "li") {
-  wrapper.innerHTML = "<ul>";
+  wrapper.innerHTML = "";
+
   picklist.forEach((list) => {
-    return (wrapper.firstChild.innerHTML += `<li>${list}<input type="button" class="removeBtn ${cls}" onclick="listRemove(this)" value="削除"></input></li>`);
+    const setAttributes = (el, attrs) => {
+      Object.keys(attrs).forEach((key) => {
+        el.setAttribute(key, attrs[key]);
+      });
+    };
+
+    const listItem = document.createElement("li");
+    const removeBtn = document.createElement("input");
+    listItem.textContent = `${list.val} [${list.lev}]`;
+    listItem.appendChild(removeBtn);
+    setAttributes(removeBtn, {
+      type: "button",
+      class: `removeBtn ${cls} pointer`,
+      onclick: "listRemove(this)",
+      value: "×",
+    });
+    setAttributes(listItem, { id: list.id });
+    wrapper.appendChild(listItem);
   });
-  pickupLists.length
-    ? (sum.innerHTML = `${pickupLists.length}/${lists.length}`)
-    : (sum.innerHTML = lists.length);
+
+  // pickupLists.length
+  //   ? (sum.innerHTML = `${pickupLists.length}/${lists.length}`)
+  sum.innerHTML = lists.length;
+
+  if (!lists.length) {
+    listWrapper.innerHTML = '<p class="list-attention">リストが登録されていません。</p>';
+  }
+
   usefulCheckbox(pickupLists);
 }
 
@@ -99,20 +119,54 @@ function num(min, max) {
 
 // 追加
 const addBtn = document.querySelector("#addBtn");
+let idNumber = 0;
 addBtn.addEventListener("click", () => {
-  let list = text.value;
-  if (list && list.match(/;|；/)) {
-    const splitValue = list.split(/;|；/);
-    lists.push(...splitValue);
-    lists = lists.filter((v) => v);
+  const list = text.value;
+  const level = document.querySelector("#level").value;
+  // if (list && list.match(/;|；/)) {
+  //   const splitValue = list.split(/;|；/);
+  //   lists.push({ val: [...splitValue], lev: level });
+  //   lists = lists.filter((v) => v);
+  //   elementPrepare(lists);
+  // } else if (list) {
+  //   lists.push({ val: list, lev: level });
+  //   elementPrepare(lists);
+  // } else {
+  //   alert("テキストフィールドに入力してください。");
+  // }
+  if (list) {
+    lists.push({ id: idNumber, val: list, lev: level });
     elementPrepare(lists);
-  } else if (list) {
-    lists.push(list);
-    elementPrepare(lists);
+    idNumber++;
   } else {
     alert("テキストフィールドに入力してください。");
   }
 });
+
+// リスト開閉
+window.onload = () => {
+  const closeBtn = document.querySelector('#close-btn');
+  const showBtn = document.querySelector('#show-btn');
+  const listContainer = document.querySelector('#list-container');
+
+  const modalBtns = [{ btn: closeBtn, style: 'none' }, { btn: showBtn, style: 'flex' }];
+
+  modalBtns.forEach((target) => {
+    target['btn'].addEventListener('click', () => {
+      listContainer.style.display = target['style'];
+      showBtnToggle();
+    });
+  });
+
+  const showBtnToggle = () => {
+    const listContainerStyle = getComputedStyle(listContainer, '').getPropertyValue('display');
+    listContainerStyle === 'flex'
+      ? showBtn.style.display = 'none'
+      : showBtn.style.display = 'block';
+  }
+
+  showBtnToggle();
+}
 
 // シャッフル
 const listShuffle = document.querySelector("#list-shuffle");
@@ -124,46 +178,47 @@ pickupShuffle.addEventListener("click", function () {
   shuffle(pickupLists, this.textContent);
 });
 
-// ピックアップ
+// // ピックアップ
 const pickupText = document.querySelector("#pickup-text");
 pickup.addEventListener("click", () => {
   const pickNum = pickupText.value;
   if (pickNum && pickNum.match(/^[1-9]+$/)) {
-    if (pickupCheckbox.checked) {
-      sortingCheckbox(pickupLists, pickNum);
-    } else {
-      sortingCheckbox(lists, pickNum);
-    }
+    pickupCheckbox.checked
+      ? sortingCheckbox(pickupLists, pickNum)
+      : sortingCheckbox(lists, pickNum);
   } else {
     alert("【1以上の半角数字】を入力してください。");
   }
 });
 
-// リセット
-const reset = document.querySelector("#reset");
-const pickupReset = document.querySelector("#pickup-reset");
-reset.addEventListener("click", () => {
-  pickupLists.splice(0);
-  pickupText.value = shuffleWrapper.innerHTML = "";
-  commonFn();
-});
-pickupReset.addEventListener("click", () => {
-  listInsert(pickupLists, pickupWrapper, "pk");
-});
+// // リセット
+// const reset = document.querySelector("#reset");
+// const pickupReset = document.querySelector("#pickup-reset");
+// reset.addEventListener("click", () => {
+//   pickupLists.splice(0);
+//   pickupText.value = shuffleWrapper.innerHTML = "";
+//   commonFn();
+// });
+// pickupReset.addEventListener("click", () => {
+//   listInsert(pickupLists, pickupWrapper, "pk");
+// });
 
-// 配列から削除
-function arrayRemove(array, removeValue) {
-  const removeIndex = array.indexOf(removeValue);
-  array.splice(removeIndex, 1);
+// // 配列から削除
+function arrayRemove(array, removeId) {
+  const removeList = array.find((list) => {
+    return list.id === Number(removeId)
+  });
+  const index = array.indexOf(removeList);
+  array.splice(index, 1);
 }
 // 削除
 function listRemove(target) {
-  const removeValue = target.parentNode.textContent;
+  const removeId = target.parentNode.id;
   if (target.classList.contains("li")) {
-    arrayRemove(lists, removeValue);
+    arrayRemove(lists, removeId);
     listInsert(lists, listWrapper);
   } else {
-    arrayRemove(pickupLists, removeValue);
+    arrayRemove(pickupLists, removeId);
     listInsert(pickupLists, pickupWrapper, "pk");
   }
   sum.innerHTML = lists.length;
@@ -171,16 +226,21 @@ function listRemove(target) {
 // ピックアップをリストから削除
 const rfl = document.querySelector("#remove-from-list");
 rfl.addEventListener("click", () => {
-  if (
-    confirm(
-      `${pickupLists}をリストから削除します。\n（削除したリストは元に戻せません）`
-    )
-  ) {
-    pickupLists.forEach((list) => {
-      arrayRemove(lists, list);
-    });
-    pickupLists.splice(0);
-    commonFn();
+  if (pickupLists.length) {
+    const val = pickupLists.forEach((list) => {
+      return list.lav;
+    })
+    if (
+      confirm(
+        `${val}をリストから削除します。\n（削除したリストは元に戻せません）`
+      )
+    ) {
+      pickupLists.forEach((list) => {
+        arrayRemove(lists, list.id);
+      });
+      pickupLists.splice(0);
+      commonFn();
+    }
   }
 });
 
@@ -196,6 +256,95 @@ allRemove.addEventListener("click", () => {
 const commonFn = () => {
   listInsert(lists, listWrapper);
   listInsert(pickupLists, pickupWrapper, "pk");
-  sum.innerHTML = lists.length;
+  // sum.innerHTML = lists.length;
   usefulCheckbox(pickupLists);
 };
+
+// グループ分け
+const groupBtn = document.querySelector("#create-group-btn");
+groupBtn.addEventListener("click", () => {
+  const groupWrapper = document.querySelector("#group-wrapper");
+  const groupNum = document.querySelector("#group-num").value;
+
+  // テーブルを生成
+  const createTable = (array) => {
+    groupWrapper.innerHTML = "";
+    const list = lists.length;
+    const perGroup = Math.ceil(list / groupNum);
+
+    // グループの数だけ繰り返す
+    for (let i = 0; i < groupNum; i++) {
+      const table = document.createElement("table");
+      const thTr = document.createElement("tr");
+      const th = document.createElement("th");
+      th.innerText = `グループ${i + 1}`;
+      thTr.appendChild(th);
+      table.appendChild(thTr);
+
+      // 1グループあたりのリスト数分繰り返す
+      for (let d = 0; d < perGroup; d++) {
+        const tdTr = document.createElement("tr");
+        const td = document.createElement("td");
+
+        if (array[i][d]) {
+          td.textContent = `${array[i][d].val} [${array[i][d].lev}]`;
+          tdTr.appendChild(td);
+          table.appendChild(tdTr);
+        }
+        groupWrapper.appendChild(table);
+      }
+    }
+  };
+
+  // グループ分けをした配列を渡す
+  const grouping = (groupNum, array) => {
+    let flatLev = false;
+    let loopCount = 0;
+    let loopBase = 5;
+    let arr = [...array];
+    const perGroup = Math.ceil(array.length / groupNum);
+
+    // レベルがほぼ均一になるまでループ
+    while (!flatLev) {
+      loopCount++;
+      if (loopCount % 50 === 0) {
+        loopBase++;
+      }
+
+      let levSum = [];
+      let newArr = [];
+      for (let i = 0; i < array.length; i++) {
+        const r = randomNum(arr);
+        [arr[i], arr[r]] = [arr[r], arr[i]];
+      }
+
+      for (let i = 0; i < groupNum; i++) {
+        const sortGroup = arr.slice(i * perGroup, (i + 1) * perGroup);
+        newArr.push(sortGroup);
+        console.log(newArr, loopCount, loopBase);
+
+        const sum = newArr[i].reduce((a, b) => {
+          return a + b.lev;
+        }, 0);
+        levSum.push(sum);
+      }
+      const range = Math.max.apply(null, levSum) - Math.min.apply(null, levSum);
+
+      if (range < (loopBase * groupNum) / 2) {
+        flatLev = true;
+        return newArr;
+      }
+    }
+  };
+
+  // グループとリストの数を確認して発火
+  if (groupNum && groupNum.match(/^[1-9]+$/)) {
+    lists.length >= groupNum && groupNum > 1
+      ? createTable(grouping(groupNum, lists))
+      : alert(
+        `${lists.length}件のリストを${groupNum}組に分けることはできません。`
+      );
+  } else {
+    alert("【2以上の半角数字】を入力してください。");
+  }
+});
